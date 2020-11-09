c945cec67283e82841554d6f8c2bcc3fe5150d0a
Zachary Durr <zdurr22@gmail.com>
2020-11-09 17:06:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,6 +20,13 @@ class Artist
     @@song_count += 1
   end
 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+    
   def self.song_count
     @@song_count
   end
