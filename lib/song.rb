8195694f449f7dda5848ece209d97aeca12a3eaf
Zachary Durr <zdurr22@gmail.com>
2020-11-09 17:08:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,10 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist
 
   @@all = []
-
+  
   def initialize(name)
     @name = name
-    save
-  end
-
-  def save
-    @@all << self
   end
 
   def self.all
