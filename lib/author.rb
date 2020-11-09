3ff495f4c602aebe7bb735fce7b79727a54c4bf8
Zachary Durr <zdurr22@gmail.com>
2020-11-09 17:13:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,7 @@ class Author
   attr_accessor :name, :posts
 
   @@post_count = 0
-  
+
   def initialize(name)
     @name = name
     @posts =[]
@@ -11,5 +11,7 @@ class Author
   def add_post(post)
     @post = post
     @posts << self
+    @@post_count += 1
+    post.author = self
 
 end
