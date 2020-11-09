b19381d9906c81b25d0c0168f9ac526e9e37ba57
Zachary Durr <zdurr22@gmail.com>
2020-11-09 18:03:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,6 @@
 class Author
   attr_accessor :name, :posts
 
-  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -10,18 +9,16 @@ class Author
   def add_post(post)
     Post.all << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     Post.all << post
     post.author = self
-    @@post_count += 1
   end
 
   def self.post_count
-    @@post_count
+    Post.all.count
   end
 
   def posts
